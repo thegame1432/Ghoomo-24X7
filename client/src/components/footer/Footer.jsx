96a8cee69cfa50b03b59ca7082c692974c16ab35
thegame1432 <harshpandiyar1432@gmail.com>
2024-06-23 14:34:46 +0530

    update css of client

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -3,7 +3,7 @@ import "./footer.css";
 const Footer = () => {
   return (
     <div className="footer">
-      <div className="fText">Copyright © 2024 Ghoomo-24X7.</div>
+      {/* <div className="fText">Copyright © 2024 Ghoomo-24X7.</div> */}
     </div>
   );
 };
